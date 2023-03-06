b0d776c216946afbb0e0dbe9a58e4de9806261b7
Ilya Ustinov <ustin_1999@mail.ru>
2023-03-06 17:58:22 +0300

    Added Task21, Update Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -6,7 +6,7 @@
 
 Console.Write("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if(IsFiveDigit(num))
+if (IsFiveDigit(num))
 {
     string output = IsPalindrome(num) ? $"Число {num} является палиндромом" : $"Число {num} не является палиндромом";
     Console.WriteLine(output);
@@ -19,13 +19,13 @@ else
 
 bool IsFiveDigit(int number)
 {
-    if(number<0) number = -number;
-    if(number>=10000 && number<100000) return true;
+    if (number < 0) number = -number;
+    if (number >= 10000 && number < 100000) return true;
     else return false;
 }
 
 bool IsPalindrome(int fiveDigitNumber)
 {
-    if(fiveDigitNumber/10000 == fiveDigitNumber%10 && fiveDigitNumber/1000%10 == fiveDigitNumber%100/10) return true;
+    if (fiveDigitNumber / 10000 == fiveDigitNumber % 10 && fiveDigitNumber / 1000 % 10 == fiveDigitNumber % 100 / 10) return true;
     else return false;
 }
